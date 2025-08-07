ea619fd843c29052a3c0db5b5fd00aa6ebddd559
samuel-vrit <samuel@vrittechnologies.com>
2025-08-07 16:01:01 +0545

    finish second screen

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const String kApiKey = '926a4b5abc0d7da83c707e29388f3e9d';
+
+Color kThemeColor = const Color(0xff94d2bd);
+
+Color kBorderColor = const Color(0xffDEDEDE);
